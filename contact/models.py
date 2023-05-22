eef641a58bf632959ec9133d6a4d126d51a2d25b
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-05-22 18:47:10 -0300

    criando e customizando o modelAdimin

--- contact/models.py
+++ contact/models.py
@@ -14,9 +14,13 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
-    Fist_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
