7e165599e5d11e4b12652bd9a0074a331d8a5bb2
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-05-24 11:12:11 -0300

    model categorye chave estrangeira

--- contact/models.py
+++ contact/models.py
@@ -12,6 +12,12 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -22,6 +28,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+        )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
