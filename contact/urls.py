f56888db8716ebf57ba99dd88994649818469fba
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-05-29 10:53:00 -0300

    filtrando valores Q e OR

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,7 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<contact_id>/', views.contact, name='contact'),  # type: ignore
-    path('', views.index, name='index'),  # type: ignore
-
+    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
+    path('search/', views.search, name='search'),  # type:ignore
+    path('', views.index, name='index'),  # type:ignore
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects .filter(show=True)\
+    contacts = Contact.objects \
+        .filter(show=True)\
         .order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
-        'site_title': 'contatos - '
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
     }
 
     return render(
