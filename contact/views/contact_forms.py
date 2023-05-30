efcdf5edb2260cddca9777c8da155a5b6ccc5d91
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-05-30 13:21:09 -0300

    formulario dinamico

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,54 @@
+from typing import Any, Dict
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+from django import forms
+from django.core.exceptions import ValidationError
+
 from contact.models import Contact
 
 
-def create(request):
-    context = {
-    }
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+            )
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Msg error',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+        return super().clean()
+
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
     return render(
         request,
         'contact/create.html',
