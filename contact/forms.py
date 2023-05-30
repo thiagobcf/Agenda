7833540baee746b72fc21e3e4b34005e658a0fbf
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-05-30 18:08:25 -0300

    configurando widgets do formulario

---
+++ contact/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init',
+        # })
+
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+            )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Msg error',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+        return super().clean()
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,38 +1,5 @@
-from typing import Any, Dict
-from django.db.models import Q
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
-from django import forms
-from django.core.exceptions import ValidationError
-
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-            )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Msg error',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-        return super().clean()
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def create(request):
