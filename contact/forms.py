db727c267e753f26d03f6f3d289b5f1154dde902
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-01 18:42:50 -0300

    usercreation em novos usuarios

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+
 from . import models
 
 
@@ -7,7 +9,7 @@ class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
         widget=forms.FileInput(
             attrs={
-                'accept': 'image/*'
+                'accept': 'image/*',
             }
         )
     )
@@ -18,13 +20,12 @@ class ContactForm(forms.ModelForm):
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
             'picture',
-            )
+        )
 
     def clean(self):
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
-
         if first_name == last_name:
             msg = ValidationError(
                 'Primeiro nome não pode ser igual ao segundo',
@@ -36,13 +37,17 @@ class ContactForm(forms.ModelForm):
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
-
         if first_name == 'ABC':
             self.add_error(
                 'first_name',
                 ValidationError(
-                    'Msg error 2',
+                    'Veio do add_error',
                     code='invalid'
                 )
             )
+
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    ...
--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,5 @@
 # type:ignore
 from django.urls import path
-
 from contact import views
 
 app_name = 'contact'
@@ -8,10 +7,12 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-
-    # contact(CRUD)
+    # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
 ]
