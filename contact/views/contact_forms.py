bbf5d2f8726ecb19cf626e6896590fd9bcfa73f7
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-01 11:54:07 -0300

    views,url e template para delete

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -41,7 +41,7 @@ def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
         )
-    form_action = reverse('contact:update', args=(contact_id))
+    form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
         form = ContactForm(request.POST, instance=contact)
@@ -62,7 +62,7 @@ def update(request, contact_id):
         )
 
     context = {
-        'form': ContactForm(istance=contact),
+        'form': ContactForm(instance=contact),
         'form_action': form_action,
     }
 
@@ -72,3 +72,22 @@ def update(request, contact_id):
         context
     )
 
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
+    )
