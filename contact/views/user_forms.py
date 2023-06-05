8fb0ae0ff400f0105201401a818d21b0c15aa0b4
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-05 11:02:35 -0300

    usando login_requered

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 from contact.forms import RegisterForm, RegisterUpdateForm
 
@@ -26,6 +27,7 @@ def register(request):
     )
 
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
 
@@ -63,6 +65,7 @@ def login_view(request):
     )
 
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
